4e069e2105df326ecb0b4fd4d6e6d5bed9f463a0
Filip Žitný <filipzitny@gmail.com>
2024-03-13 22:52:56 +0100

    pylint

--- backend_test/backend_test/models.py
+++ backend_test/backend_test/models.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 """Models for Rest api."""
-from django.db import models
+import django.db.models
 
 
-class User(models.Model):
+class User(django.db.models.Model):
     """Class users."""
 
-    name = models.CharField(max_length=255)
-    registered_at = models.DateTimeField()
+    def __init__(self):
+        """Initialize."""
+        self.name = django.db.models.CharField(max_length=255)
+        self.registered_at = django.db.models.DateTimeField()
 
     def __str__(self):
         """Return name."""
         return self.name
 
+    def __doc__(self):
+        """Return documentation."""
+        return self.name.__doc__
